77a613b1782c5f43dfd7fb1c3160282c962b3f83
Konstantinos Dimitropoulos <kostasdmtropoulos@gmail.com>
2025-08-31 17:36:33 +0300

    Basic data exploration and Frequency of attrition

--- Code.R
+++ Code.R
@@ -6,4 +6,15 @@ hr_data <- read.csv("Employee-Attriction-Dataset.csv")
 # Packages 
 
 
-#Code
+# Basic data exploration
+
+dim(hr_data)
+str(hr_data)
+summary(hr_data)
+colSums(is.na(hr_data))
+
+
+# Frequency of attrition
+
+table(hr_data$Attrition)
+prop.table(table(hr_data$Attrition))
