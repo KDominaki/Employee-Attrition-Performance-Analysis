eeaee9dd12ce71a6d6051e2340c02be16968d682
Konstantinos Dimitropoulos <kostasdmtropoulos@gmail.com>
2025-08-31 17:53:13 +0300

    Plots

--- Code.R
+++ Code.R
@@ -5,6 +5,9 @@ hr_data <- read.csv("Employee-Attriction-Dataset.csv")
 
 # Packages 
 
+install.packages("ggplot2")
+library(ggplot2)
+
 
 # Basic data exploration
 
@@ -18,3 +21,25 @@ colSums(is.na(hr_data))
 
 table(hr_data$Attrition)
 prop.table(table(hr_data$Attrition))
+
+
+
+# Plots 
+
+#Gender/Attrition
+ggplot(hr_data, aes(x=Attrition, fill=Gender)) + geom_bar()
+
+#Overtime/Attrition
+ggplot(hr_data, aes(x=Attrition, fill=OverTime)) + geom_bar()
+
+#Age/Attrition
+ggplot(hr_data, aes(x=Age, fill=Attrition)) + geom_histogram(bins=20, position="dodge")
+
+#Salary/Attrition
+ggplot(hr_data, aes(x=Attrition, y=MonthlyIncome, fill=Attrition)) + geom_boxplot()
+
+
+
+
+
+
